a636bfd8d49f9e295dff47de6dd043ee97450e2f
Micael Jarniac <micael@jarniac.com>
2023-04-28 00:28:19 -0300

    feat: add dependencies to `pyproject.toml`
    Closes #111
    Closes #103

--- {{cookiecutter.project_slug}}/docs/CONTRIBUTING.md
+++ {{cookiecutter.project_slug}}/docs/CONTRIBUTING.md
@@ -64,7 +64,7 @@ Here is a summary of the steps to follow:
 $ git checkout main
 $ git pull upstream main
 $ pip install -U -r requirements.txt
-$ pip install -U -r requirements-dev.txt
+$ pip install -U -e .[dev]
 ```
 3. Create a new topic branch (off the main project development branch) to contain your feature, change, or fix:
 ```bash
@@ -221,7 +221,7 @@ source venv/bin/activate
 
 # Install the dependencies
 pip install -U -r requirements.txt
-pip install -U -r requirements-dev.txt
+pip install -U -e .[dev]
 
 # Set up pre-commit hooks
 pre-commit install
@@ -275,7 +275,8 @@ make html
 
 ### Requirements
 
-Packages that are necessary for the code to run should be on [`requirements.txt`](../requirements.txt), and those only necessary while developing should be on [`requirements-dev.txt`](../requirements-dev.txt).
+All requirements should be on [`pyproject.toml`](../pyproject.toml), on their respective sections.
+Direct dependencies should have an open version range there, and have their versions pinned on [`requirements.txt`](../requirements.txt).
 
 To see which installed packages are outdated, run `pip list --outdated`.
 
--- {{cookiecutter.project_slug}}/noxfile.py
+++ {{cookiecutter.project_slug}}/noxfile.py
@@ -3,12 +3,16 @@
 import nox
 
 python_versions = ["3.7", "3.8", "3.9", "3.10"]
+deps = {
+    "ruff": "ruff==0.0.263",
+    "black": "black==23.3.0",
+}
 
 
 @nox.session(python=python_versions[-1], reuse_venv=True)
 def format_files(session: nox.Session) -> None:
     """Format files."""
-    session.install("-r", "requirements-dev.txt")
+    session.install(deps["ruff"], deps["black"])
     session.run("ruff", "check", "--fix-only", ".")
     session.run("black", ".")
 
@@ -16,21 +20,19 @@ def format_files(session: nox.Session) -> None:
 @nox.session(python=python_versions[-1], reuse_venv=True)
 def lint_files(session: nox.Session) -> None:
     """Lint files."""
-    session.install("-r", "requirements-dev.txt")
+    session.install(deps["ruff"])
     session.run("ruff", "check", ".")
 
 
 @nox.session(python=python_versions, reuse_venv=True)
 def type_check_code(session: nox.Session) -> None:
     """Type-check code."""
-    session.install("-r", "requirements-dev.txt")
-    session.install(".")
+    session.install(".[dev]")
     session.run("mypy")
 
 
 @nox.session(python=python_versions)
 def test_code(session: nox.Session) -> None:
     """Test code."""
-    session.install(".")
-    session.install("pytest")
+    session.install(".[tests]")
     session.run("pytest")
