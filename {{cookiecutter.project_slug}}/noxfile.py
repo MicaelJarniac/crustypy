d109d9da64294b8cdb61d37b7550379d19378fde
Micael Jarniac <micael@jarniac.com>
2023-05-03 14:20:50 -0300

    feat: constraints

--- {{cookiecutter.project_slug}}/noxfile.py
+++ {{cookiecutter.project_slug}}/noxfile.py
@@ -3,16 +3,13 @@
 import nox
 
 python_versions = ["3.7", "3.8", "3.9", "3.10"]
-deps = {
-    "ruff": "ruff==0.0.263",
-    "black": "black==23.3.0",
-}
+constraints = ("-c", "constraints.txt")
 
 
 @nox.session(python=python_versions[-1], reuse_venv=True)
 def format_files(session: nox.Session) -> None:
     """Format files."""
-    session.install(deps["ruff"], deps["black"])
+    session.install(*constraints, "ruff", "black")
     session.run("ruff", "check", "--fix-only", ".")
     session.run("black", ".")
 
@@ -20,19 +17,19 @@ def format_files(session: nox.Session) -> None:
 @nox.session(python=python_versions[-1], reuse_venv=True)
 def lint_files(session: nox.Session) -> None:
     """Lint files."""
-    session.install(deps["ruff"])
+    session.install(*constraints, "ruff")
     session.run("ruff", "check", ".")
 
 
 @nox.session(python=python_versions, reuse_venv=True)
 def type_check_code(session: nox.Session) -> None:
     """Type-check code."""
-    session.install(".[dev]")
+    session.install(*constraints, ".[dev]")
     session.run("mypy")
 
 
 @nox.session(python=python_versions)
 def test_code(session: nox.Session) -> None:
     """Test code."""
-    session.install(".[tests]")
+    session.install(*constraints, ".[tests]")
     session.run("pytest")
