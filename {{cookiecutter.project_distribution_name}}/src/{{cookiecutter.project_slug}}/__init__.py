a756a25c203f61437431056356ee41d83beecabf
Micael Jarniac <micael@jarniac.com>
2023-06-19 22:06:43 -0300

    chore: move __all__ to top

--- {{cookiecutter.project_distribution_name}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_distribution_name}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,7 +1,6 @@
 """{{ cookiecutter.project_short_description }}"""
 
+__all__ = ["make_greeting"]
 __version__ = "{{ cookiecutter.version }}"
 
 from ._{{ cookiecutter.project_slug }} import make_greeting
-
-__all__ = ["make_greeting"]
