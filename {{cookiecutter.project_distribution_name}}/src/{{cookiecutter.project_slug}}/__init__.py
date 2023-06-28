3be0af144815ae2c5fab6d6b30eb702e116a0717
Micael Jarniac <micael@jarniac.com>
2023-06-28 15:53:35 -0300

    fix(build): version variable

--- {{cookiecutter.project_distribution_name}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_distribution_name}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,6 +1,5 @@
 """{{ cookiecutter.project_short_description }}"""
 
 __all__ = ["make_greeting"]
-__version__ = "{{ cookiecutter.version }}"
 
 from ._{{ cookiecutter.project_slug }} import make_greeting
